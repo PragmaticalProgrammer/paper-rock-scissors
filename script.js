b469ea0764e56acbd9efd3e18e64d1a60cd9b0de
PragmaticalProgrammer <cesar.rojas7563@outlook.com>
2022-02-25 15:49:07 -0600

    feat: added functional UI to the game keeping track of score, and resetting after five rounds

--- script.js
+++ script.js
@@ -1,52 +1,202 @@
-function computerPlay() {
-  let choices = ["paper", "rock", "scissors"];
-  let choice = choices[Math.floor(Math.random() * choices.length)];
-  return choice;
-}
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+let rounds = 0;
 
-function userPlay() {
-  let choice = prompt("Rock, Paper or Scissors? ").toLowerCase();
-  return choice;
-}
+function result(computerObjet, player, roundResult) {
+  let computer = Math.floor(Math.random() * 3);
+  choices = ["paper", "rock", "scissors"];
 
-function playRound(user, computer) {
   if (
-    (user === "paper" && computer === "rock") ||
-    (user == "rock" && computer === "scissors") ||
-    (user === "scissors" && computer === "paper")
+    (player === "paper" && choices[computer] === "rock") ||
+    (player == "rock" && choices[computer] === "scissors") ||
+    (player === "scissors" && choices[computer] === "paper")
   ) {
-    console.log(`You win, ${user} beats ${computer}`);
+    roundResult.textContent = `Round Result: You win, ${player} beats ${choices[computer]}`;
+    computerObjet.style.backgroundImage = `url(images/${choices[computer]}.png)`;
     return 1;
-  } else if (user == computer) {
-    console.log(`It's a tie, you both picked ${user}`);
-    return;
+  } else if (player == choices[computer]) {
+    roundResult.textContent = `Round Result: It's a tie, you both picked ${player}`;
+    computerObjet.style.backgroundImage = `url(images/${choices[computer]}.png)`;
+    return 3;
   } else {
-    console.log(`You lose, ${computer} beats ${user}`);
+    computerObjet.style.backgroundImage = `url(images/${choices[computer]}.png)`;
+    roundResult.textContent = `Round Result: You lose, ${choices[computer]} beats ${player}`;
     return 2;
   }
 }
 
-function game() {
-  let compScore = 0;
-  let userScore = 0;
-
-  for (i = 0; i < 5; i++) {
-    let result = playRound(userPlay(), computerPlay());
-
-    if (result === 1) {
-      userScore += 1;
-    } else if (result == 2) {
-      compScore += 1;
-    }
-  }
-
-  if (userScore > compScore) {
-    return `You won with a final score of ${userScore} and the computer had ${compScore}`;
-  } else if (compScore > userScore) {
-    return `You lost with a final score of ${userScore} and the computer had ${compScore}`;
+function playGame(computer, player, status, roundResult) {
+  if (status) {
+    computer.classList.add("rainbow-color");
+    computer.style.backgroundImage = "url(images/question-mark.png)";
   } else {
-    return `It's a tie! Final score for both ${userScore}`;
+    computer.classList.remove("rainbow-color");
+    return result(computer, player, roundResult);
   }
 }
 
-console.log(game());
+const computerChoice = document.querySelector("#computerChoice");
+const playerChoiceOne = document.querySelector("#playerChoiceOne");
+const playerChoiceTwo = document.querySelector("#playerChoiceTwo");
+const playerChoiceThree = document.querySelector("#playerChoiceThree");
+const spanPlayerScore = document.querySelector("#spanPlayerScore");
+const spanComputerScore = document.querySelector("#spanComputerScore");
+const spanTies = document.querySelector("#spanTies");
+const currentRound = document.querySelector("#currentRound");
+const roundResult = document.querySelector("#roundResult");
+const matchResult = document.querySelector("#matchResult");
+
+playerChoiceOne.addEventListener("click", function () {
+  playGame(computerChoice, "paper", true, roundResult);
+  roundResult.textContent = "Round Result: ";
+  playerChoiceOne.style.pointerEvents = "none";
+  playerChoiceTwo.style.pointerEvents = "none";
+  playerChoiceThree.style.pointerEvents = "none";
+  if (rounds + 1 > 5) {
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+    rounds = 0;
+    rounds++;
+  } else {
+    rounds++;
+  }
+  currentRound.textContent = `Round: ${rounds}`;
+  setTimeout(function () {
+    let score = playGame(computerChoice, "paper", false, roundResult);
+    if (score == 1) {
+      playerScore++;
+    } else if (score == 2) {
+      computerScore++;
+    } else {
+      ties++;
+    }
+    console.log(`player score: ${playerScore}`);
+    spanPlayerScore.textContent = playerScore;
+    console.log(`computer score: ${computerScore}`);
+    spanComputerScore.textContent = computerScore;
+    console.log(`ties: ${ties}`);
+    spanTies.textContent = ties;
+    playerChoiceOne.style.pointerEvents = "auto";
+    playerChoiceTwo.style.pointerEvents = "auto";
+    playerChoiceThree.style.pointerEvents = "auto";
+
+    if (rounds + 1 == 6) {
+      if (playerScore > computerScore) {
+        matchResult.textContent = "Match Result: PLAYER WINS!";
+      } else if (computerScore > playerScore) {
+        matchResult.textContent = "Match Result: Computer WINS!";
+      } else if (playerScore == computerScore || ties == 5) {
+        matchResult.textContent = "Match Result: It was a tie!";
+      }
+    }
+  }, 3000);
+});
+
+playerChoiceTwo.addEventListener("click", function () {
+  playGame(computerChoice, "rock", true, roundResult);
+  roundResult.textContent = "Round Result: ";
+  playerChoiceOne.style.pointerEvents = "none";
+  playerChoiceTwo.style.pointerEvents = "none";
+  playerChoiceThree.style.pointerEvents = "none";
+  if (rounds + 1 > 5) {
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+    rounds = 0;
+    rounds++;
+  } else {
+    rounds++;
+  }
+
+  currentRound.textContent = `Round: ${rounds}`;
+  setTimeout(function () {
+    let score = playGame(computerChoice, "rock", false, roundResult);
+    if (score == 1) {
+      playerScore++;
+    } else if (score == 2) {
+      computerScore++;
+    } else {
+      ties++;
+    }
+    console.log(`player score: ${playerScore}`);
+    spanPlayerScore.textContent = playerScore;
+    console.log(`computer score: ${computerScore}`);
+    spanComputerScore.textContent = computerScore;
+    console.log(`ties: ${ties}`);
+    spanTies.textContent = ties;
+
+    if (rounds + 1 == 6) {
+      if (playerScore > computerScore) {
+        matchResult.textContent = "Match Result: PLAYER WINS!";
+      } else if (computerScore > playerScore) {
+        matchResult.textContent = "Match Result: Computer WINS!";
+      } else if (playerScore == computerScore || ties == 5) {
+        matchResult.textContent = "Match Result: It was a tie!";
+      }
+    }
+    playerChoiceOne.style.pointerEvents = "auto";
+    playerChoiceTwo.style.pointerEvents = "auto";
+    playerChoiceThree.style.pointerEvents = "auto";
+  }, 3000);
+});
+
+playerChoiceThree.addEventListener("click", function () {
+  playGame(computerChoice, "scissors", true, roundResult);
+  roundResult.textContent = "Round Result: ";
+  playerChoiceOne.style.pointerEvents = "none";
+  playerChoiceTwo.style.pointerEvents = "none";
+  playerChoiceThree.style.pointerEvents = "none";
+  if (rounds + 1 > 5) {
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+    rounds = 0;
+    rounds++;
+  } else {
+    rounds++;
+  }
+
+  currentRound.textContent = `Round: ${rounds}`;
+  setTimeout(function () {
+    let score = playGame(computerChoice, "scissors", false, roundResult);
+    if (score == 1) {
+      playerScore++;
+    } else if (score == 2) {
+      computerScore++;
+    } else {
+      ties++;
+    }
+
+    if (rounds + 1 == 6) {
+      if (playerScore > computerScore) {
+        matchResult.textContent = "Match Result: PLAYER WINS!";
+      } else if (computerScore > playerScore) {
+        matchResult.textContent = "Match Result: Computer WINS!";
+      } else if (playerScore == computerScore || ties == 5) {
+        matchResult.textContent = "Match Result: It was a tie!";
+      }
+    }
+
+    console.log(`player score: ${playerScore}`);
+    spanPlayerScore.textContent = playerScore;
+    console.log(`computer score: ${computerScore}`);
+    spanComputerScore.textContent = computerScore;
+    console.log(`ties: ${ties}`);
+    spanTies.textContent = ties;
+
+    if (rounds + 1 == 6) {
+      if (playerScore > computerScore) {
+        matchResult.textContent = "Match Result: PLAYER WINS!";
+      } else if (computerScore > playerScore) {
+        matchResult.textContent = "Match Result: Computer WINS!";
+      } else if (playerScore == computerScore || ties == 5) {
+        matchResult.textContent = "Match Result: It was a tie!";
+      }
+    }
+    playerChoiceOne.style.pointerEvents = "auto";
+    playerChoiceTwo.style.pointerEvents = "auto";
+    playerChoiceThree.style.pointerEvents = "auto";
+  }, 3000);
+});
